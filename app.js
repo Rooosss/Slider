f79e3fc27b4e8e3aff4fe58d839decc81c439e30
Rooosss <vyshemirskyirostyslav@gmail.com>
2020-04-18 18:55:19 +0300

    code editing

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 let slides = document.querySelectorAll(".myslides"),
-	dots = document.querySelectorAll(".dot"),
-	dotsArea = document.querySelector(".dots"),
-	prev = document.querySelector(".prev"),
-	next = document.querySelector(".next"),
-	number = document.querySelectorAll(".numbertext"),
-	img = document.querySelectorAll("img");
-	slideindex = 1;
+  dots = document.querySelectorAll(".dot"),
+  dotsArea = document.querySelector(".dots"),
+  prev = document.querySelector(".prev"),
+  next = document.querySelector(".next"),
+  number = document.querySelectorAll(".numbertext"),
+  img = document.querySelectorAll("img");
+  slideindex = 1;
 
 function showSlide(n) {
-	if (n < 1) {
-		slideindex = slides.length;
-	} else if (n > slides.length) {
-		slideindex = 1;
-	}
+  if (n < 1) {
+    slideindex = slides.length;
+  } else if (n > slides.length) {
+    slideindex = 1;
+  }
 
-	for (let i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
     dots[i].classList.remove("active");
     slides[slideindex - 1].style.display = "block";
     dots[slideindex - 1].classList.add("active");
-	}
-
-	prev.onclick = function() {
-		showSlide(slideindex += -1);
-	}
-
-  next.onclick = function() {
-    showSlide(slideindex += 1);
   }
   setSlideNumber();
 }
 showSlide(slideindex);
 
+prev.onclick = function() {
+  showSlide(slideindex += -1);
+}
+
+next.onclick = function() {
+  showSlide(slideindex += 1);
+}
+
 function setSlideNumber() {
-	for (let i = 0; i < number.length; i++) {
-		number[i].innerHTML = slideindex + "/" + number.length;
-		img[i].src = "css/image/img" + slideindex + ".jpg";
-	}
+  for (let i = 0; i < number.length; i++) {
+    number[i].innerHTML = slideindex + "/" + number.length;
+    img[i].src = "css/image/img" + slideindex + ".jpg";
+  }
 }
 
 dotsArea.onclick = function(e) {
