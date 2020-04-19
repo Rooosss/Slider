12437937ae550ce9098c3ae220babe0e06791113
Rooosss <60131265+Rooosss@users.noreply.github.com>
2020-04-19 16:52:19 +0300

    Improve slider
    * change Slider and add styles
    
    * changed slide number counter
    
    * removed unnecessary functions
    
    * code editing
    
    * changed class names
    
    * indentation for variables added

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
+let slides = document.querySelectorAll(".my-slides"),
+    dots = document.querySelectorAll(".dot"),
+    dotsArea = document.querySelector(".dots"),
+    prev = document.querySelector(".prev"),
+    next = document.querySelector(".next"),
+    number = document.querySelectorAll(".number-text"),
+    img = document.querySelectorAll("img");
+    slideindex = 1;
 
-let slides = document.querySelectorAll(".myslides"),
-	dots = document.querySelectorAll(".dot"),
-	prev = document.querySelector(".prev"),
-	next = document.querySelector(".next"),
-	number = document.querySelectorAll(".numbertext"),
-	slideindex = 1;
+function showSlide(n) {
+  if (n < 1) {
+    slideindex = slides.length;
+  } else if (n > slides.length) {
+    slideindex = 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    dots[i].classList.remove("active-dot");
+    slides[slideindex - 1].classList.add("visible");
+    dots[slideindex - 1].classList.add("active-dot");
+  }
+  setSlideNumber();
+}
 
 showSlide(slideindex);
 
-function showSlide(n) {
-	if (n < 1) {
-		slideindex = slides.length;
-	} else if (n > slides.length) {
-		slideindex = 1;
-	}
-	for (let i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	for (let i = 0; i < dots.length; i++) {
-		dots[i].classList.remove("active");
-	}
-	
-	slides[slideindex - 1].style.display = "block";
-	dots[slideindex - 1].classList.add("active");
-	counter();
-}
-
-function counter() {
-	for (let i = 0; i < number.length; i++) {
-		number[i].innerHTML = slideindex + "/3";
-	}
-}
-
-function plusSlide(n) {
-	showSlide(slideindex += n);
-}
-
 prev.onclick = function() {
-	plusSlide(1);
+  showSlide(slideindex -= 1);
 }
 
 next.onclick = function() {
-	plusSlide(-1);
+  showSlide(slideindex += 1);
 }
 
-function currentSlide(n) {
-	showSlide(slideindex = n);
+function setSlideNumber() {
+  for (let i = 0; i < number.length; i++) {
+    number[i].innerHTML = slideindex + "/" + number.length;
+    img[i].src = "css/image/img" + slideindex + ".jpg";
+  }
 }
 
-
-
+dotsArea.onclick = function(e) {
+  for (let i = 0; i < dots.length + 1; i++) {
+    if (e.target == dots[i - 1]) {
+      showSlide(slideindex = i);
+    }
+  }
+}
